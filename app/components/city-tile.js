cee2d1cbdee5872c70cefed26345cff009f19ed9
Katarina Tuttle and Kyle Helyar <katarina+kyle@>
2016-08-18 11:05:03 -0700

    one to many

---
+++ app/components/city-tile.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    destroyCity(city) {
+      if (confirm('Are you sure you want to delete this city?')) {
+        this.sendAction('destroyCity', city);
+      }
+    }
+});
